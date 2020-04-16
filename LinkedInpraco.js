1e07a60d19c940dc5f6affe5529177e181afd555
Palak Agarwal <palakag1410@gmail.com>
2020-04-16 20:30:32 +0530

    Add files via upload

---
+++ LinkedInpraco.js
@@ -0,0 +1,16 @@
+
+<!--Javascript code for LinkedIn likes-->
+
+var count= document.getElementsByClassName("react-button__trigger artdeco-button artdeco-button--muted artdeco-button--4 artdeco-button--tertiary ember-view")
+for(var i=0;i<count.length;i++)
+{
+count[i].click();
+}
+
+<!-- Javascript code for LinkedIn connections-->
+
+var count= document.getElementsByClassName("full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view")
+for(var i=0;i<count.length;i++)
+{
+count[i].click();
+}
